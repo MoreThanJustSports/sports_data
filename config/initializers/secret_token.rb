74f4fa9841f941199a7468d1bc2b8ea324d70697
Jorge Arguelles <jorgeav@gmail.com>
2013-05-04 18:01:21 -0400

    Initial Rails Project Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportsData::Application.config.secret_token = '81b359bc5eabfacc60fff0b2cdc65b9a1037e39fe650cc0f0813799b670b0c87545a0e984fb0cec11405f5199ec7f3dbc1ad14b8724313c11eca1e6d5a56c275'
